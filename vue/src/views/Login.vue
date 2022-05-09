28e83277cd3a334729dfa88555e9adfb1d274d64
vilgefo <pavellancov@outlook.com>
2022-05-10 00:08:11 +0400

    implement login error message

--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -14,11 +14,22 @@
         </p>
     </div>
     <form class="mt-8 space-y-6" @submit="login">
+        <div v-if="errorMsg" class="flex items-center justify-between py-3 px-5 bg-red-500 text-white rounded">
+            {{ errorMsg }}
+            <span @click="errorMsg = ''"
+                  class="w-8 h-8 flex items-center justify-center rounded-full transition-colors cursor-pointer hover:bg-[rgba(0,0,0,0.2)]">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"/>
+                </svg>
+            </span>
+        </div>
+
         <input type="hidden" name="remember" value="true"/>
         <div class="rounded-md shadow-sm -space-y-px">
             <div>
                 <label for="email-address" class="sr-only">Email address</label>
-                <input id="email-address" name="email" type="email" autocomplete="email" required="" v-model="user.email"
+                <input id="email-address" name="email" type="email" autocomplete="email" required=""
+                       v-model="user.email"
                        class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                        placeholder="Email address"/>
             </div>
@@ -55,9 +66,10 @@
 </template>
 
 <script setup>
-import { LockClosedIcon } from '@heroicons/vue/solid'
+import {LockClosedIcon} from '@heroicons/vue/solid'
 import store from "../store";
 import {useRouter} from "vue-router";
+import {ref} from "vue";
 
 const router = useRouter();
 const user = {
@@ -65,12 +77,19 @@ const user = {
     password: '',
     remember: false
 }
-function login(ev){
+
+let errorMsg = ref('');
+
+function login(ev) {
     ev.preventDefault()
-    store.dispatch('login', user).then(()=>{
+
+
+    store.dispatch('login', user).then(() => {
         router.push({
             name: 'Dashboard'
         })
+    }).catch(err => {
+        errorMsg.value = err.response.data.error;
     })
 }
 </script>
