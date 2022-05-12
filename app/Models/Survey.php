384bab9990fc4628d84aaa389b5d35f3ae59125b
vilgefo <pavellancov@outlook.com>
2022-05-12 13:29:44 +0400

    add Survey CRUD

--- app/Models/Survey.php
+++ app/Models/Survey.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+use Spatie\Sluggable\SlugOptions;
+use Spatie\Sluggable\HasSlug;
 
+/**
+ * @mixin Builder
+ */
 class Survey extends Model
 {
-    use HasFactory;
+    use HasFactory, HasSlug;
+
+    protected $fillable = ['user_id', 'status', 'slug', 'status', 'description', 'expire_date'];
+
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()->generateSlugsFrom('title')->saveSlugsTo('slug');
+    }
 }
